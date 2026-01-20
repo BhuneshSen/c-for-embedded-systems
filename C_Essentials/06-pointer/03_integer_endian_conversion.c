b54864ecd39e3430d716d30715a3498696c0b513
Bhunesh Sen <bhuneshsen761@gmail.com>
2026-01-20 23:30:24 +0530

    Update 03_integer_endian_conversion.c

--- C_Essentials/06-pointer/03_integer_endian_conversion.c
+++ C_Essentials/06-pointer/03_integer_endian_conversion.c
@@ -22,6 +22,18 @@ int main(void)
 /* Function Definition */
 unsigned int swapEndian32(unsigned int num)
 {
+/*
+    // Approach
+    unsigned int b1, b2, b3, b4;
+
+    b1 = (num & 0x000000FF) << 24;  // lowest byte → highest
+    b2 = (num & 0x0000FF00) << 8;   // 2nd byte → 3rd
+    b3 = (num & 0x00FF0000) >> 8;   // 3rd byte → 2nd
+    b4 = (num & 0xFF000000) >> 24;  // highest byte → lowest
+
+    return b1 | b2 | b3 | b4;
+*/
+    
     return ((num >> 24) & 0x000000FF) |
            ((num >> 8)  & 0x0000FF00) |
            ((num << 8)  & 0x00FF0000) |
